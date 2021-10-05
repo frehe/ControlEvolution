9aac06a070f3e8cfcfb98b28d2d00a569cf460fa
heetmeyer <heerfn@yahoo.de>
2021-10-05 21:26:27 +0200

    Extend problem with cost penalty Q

--- OptimizationProblems/quadratic.py
+++ OptimizationProblems/quadratic.py
@@ -4,6 +4,7 @@ from OptimizationProblems import OptimizationProblem
 class Quadratic(OptimizationProblem):
     def __init__(self, n: int) -> None:
         super().__init__(n)
+        self.Q = np.eye(n)
         
     def function(self, x: np.ndarray) -> np.float:
-        return x.transpose() * x
+        return x.transpose().dot(self.Q.dot(x))
