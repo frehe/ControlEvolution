273b8657a94be50279db6ef9c7d7cc77f6d577d3
heetmeyer <heerfn@yahoo.de>
2021-10-05 21:26:50 +0200

    Use cost penalty Q from optimization problem class

--- Controllers/Controller.py
+++ Controllers/Controller.py
@@ -1,18 +1,21 @@
 from abc import ABC
 import numpy as np
-from numpy.linalg.linalg import solve
+
 from scipy.linalg import solve_discrete_are
 
+from OptimizationProblems import OptimizationProblem
+
 class Controller(ABC):
-    def __init__(self) -> None:
+    def __init__(self, optimization_problem: OptimizationProblem) -> None:
         super().__init__()
+        self.optimization_problem = optimization_problem
     
     def step(self, state: np.ndarray) -> np.ndarray:
         # LQR controller
         pop_size, n = state.shape
         A = np.eye(n)
         B = 0.1 * np.eye(n)
-        Q = np.eye(n)
+        Q = self.optimization_problem.Q
         R = np.zeros((n, n))
 
         P = solve_discrete_are(A, B, Q, R)
